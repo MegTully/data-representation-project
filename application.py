01bf94d229ae3e032e546cc93a02ccba9e281737
MegTully <g00411235@gmit.ie>
2022-12-22 17:46:06 +0000

    html stuff and more flask stuff

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 
 app = Flask(__name__,static_url_path='',static_folder='static')
 
@@ -30,6 +30,46 @@ def findById(id):
         return jsonify({}), 204
     return jsonify(foundMovies[0])
 
+# create a new entry for Christmas movie
+@app.route('/ChristmasMovies', methods = ['POST'])
+def createChristmasMovie():
+    global NextId
+    # Check that the user created an entry and if they didn't send an error
+    if not request.json:
+        abort(400)
+
+    ChristmasMovie = {
+        'id':NextId,
+        'title':'test',
+        'genre':'test'
+    }
+    ChristmasMovies.append(ChristmasMovie)
+    NextId += 1
+    return jsonify(ChristmasMovie)
+
+# Update an entry in the christmas movie list by searching the Id then changing the values
+@app.route('/ChristmasMovies/<int:id>', methods=['PUT'])
+def UpdateById(id):
+    foundMovies = list(filter(lambda t: t["id"]== id, ChristmasMovies))
+    if len(foundMovies)== 0:
+        return jsonify({}), 404
+    currentMovie = foundMovies[0]
+    if 'title' in request.json:
+        currentMovie['title'] = request.json['title']
+    if 'genre' in request.json:
+        currentMovie['genre'] = request.json['genre']
+
+    return jsonify(currentMovie)
+
+# Find and entry in the christmas movie list by searching its id and then delete it
+@app.route('/ChristmasMovies/<int:id>', methods=['DELETE'])
+def DeleteById(id):
+    foundMovies = list(filter(lambda t: t["id"]==id, ChristmasMovies))
+    if len(foundMovies)==0:
+        return jsonify({}), 404
+    ChristmasMovies.remove(foundMovies[0])
+
+    return jsonify({"done":True})
 @app.route('/rate/<ChristmasMoviesTitle>',methods=['POST'])
 def rateMovies(ChristmasMoviesTitle):
     return jsonify({'title': ChristmasMoviesTitle})
